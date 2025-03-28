42666f462a4ca9e9ea00d2b196b88ee9133d55ae
Devan Huapaya <huapayadevan@gmail.com>
2025-03-28 12:32:03 -0700

    chore: add barrel file for functions

---
+++ functions/index.sh
@@ -0,0 +1,8 @@
+# Barrel file for functions
+source $dotfile_dir/functions/localhost.sh
+source $dotfile_dir/functions/aliases.sh
+source $dotfile_dir/functions/github.sh
+source $dotfile_dir/functions/facebook.sh
+source $dotfile_dir/functions/random.sh
+source $dotfile_dir/functions/node.sh
+source $dotfile_dir/functions/new-function.sh
--- index.sh
+++ index.sh
@@ -4,13 +4,8 @@ dotfile_dir=~/dotfiles
 # Variables used everywhere
 source $dotfile_dir/vars/colors.sh
 
-# Functions loosely organized category
-source $dotfile_dir/functions/localhost.sh
-source $dotfile_dir/functions/aliases.sh
-source $dotfile_dir/functions/github.sh
-source $dotfile_dir/functions/facebook.sh
-source $dotfile_dir/functions/random.sh
-source $dotfile_dir/functions/node.sh
+# Import functions
+source $dotfile_dir/functions/index.sh
 
 # A bunch of aliases
 source $dotfile_dir/aliases.sh
