e8e1c875e7f17445afecb968a233a04fa551050a
Devan Huapaya <huapayadevan@gmail.com>
2025-06-11 14:41:47 -0700

    chore: update git-clone

--- functions/github.sh
+++ functions/github.sh
@@ -2,10 +2,25 @@ source $dotfile_dir/.gitVars
 DEFAULT_BRANCH='development'
 DEFAULT_ORIGIN='origin'
 
+# Helper function to get the name of a repo
+get_repo_name() {
+  local url="$1"
+  local repo_name
+
+  # Remove .git suffix if present
+  url="${url%.git}"
+
+  # Extract the part after the last slash or colon
+  repo_name="${url##*/}"
+  repo_name="${repo_name##*:}"
+
+  echo "$repo_name"
+}
+
 # Calls git add, git commit, and git push
 # Takes commit message
 function git-add-push() {
-    git add . &&
+  git add . &&
     git commit -m "${@}" &&
     git push
 }
@@ -13,9 +28,9 @@ alias gap="git-add-push"
 
 # Pulls down pull request from upstream and creates branch
 function pull-request() {
-    git fetch upstream pull/${1}/head:pr-${1} &&
+  git fetch upstream pull/${1}/head:pr-${1} &&
     git checkout pr-${1} &&
-    git remote prune upstream;
+    git remote prune upstream
 }
 alias pr="pull-request"
 
@@ -40,16 +55,16 @@ function new-repo() {
 }
 alias gn='new-repo'
 
-function git-reset(){
+function git-reset() {
   git fetch
   _BRANCH=${DEFAULT_BRANCH}
   _ORIGIN=${DEFAULT_ORIGIN}
-  
-  if [ -d "$1"]; then 
-      _BRANCH=$1
-      if [ -d "$2"]; then 
-        _ORIGIN=$2
-      fi
+
+  if [ -d "$1"]; then
+    _BRANCH=$1
+    if [ -d "$2"]; then
+      _ORIGIN=$2
+    fi
   fi
 
   echo `git reset --hard ${DEFAULT_ORIGIN}/${DEFAULT_BRANCH}`
@@ -57,13 +72,34 @@ function git-reset(){
 }
 alias gr!="git-reset"
 
-function git-clone(){
-  if [[ $1 == *"git@"* ]]; then
-    echo `git clone ${1}`
-    git clone ${1}
-  else 
-    echo `git clone ${GIT_SSH_URL}${1}.git`
-    git clone ${GIT_SSH_URL}${1}.git
+function git-clone() {
+  one-arg-required "$@" || return 1
+
+  # TODO: accommodate for ssh
+  # Clone the repo
+  # if [[ $1 == *"git@"* ]]; then
+  #   echo $(git clone ${1})
+  #   git clone ${1}
+  # else
+  #   echo $(git clone ${GIT_SSH_URL}${1}.git)
+  #   git clone ${GIT_SSH_URL}${1}.git
+  # fi
+
+  # Clone the repo
+  git clone "$@"
+
+  # Navigate to the directory
+  repo_name=""
+
+  # If only one argument is passed
+  if [ "$#" -eq 1 ]; then
+    repo_name=$(get_repo_name ${1})
+  # Otherwise use the second argument
+  elif [ "$#" -eq 2 ]; then
+    repo_name=$(get_repo_name ${2})
   fi
+
+  # Navigate to repo
+  cd $repo_name
 }
 alias gcl="git-clone"
