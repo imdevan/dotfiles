44beebf3664d564b93dc689b5f21ff0f4c87c5d6
Devan Huapaya <huapayadevan@gmail.com>
2025-06-13 19:44:12 -0700

    update alias and bookmark functions

--- functions/aliases.sh
+++ functions/aliases.sh
@@ -1,5 +1,3 @@
-
-
 # Default editor
 editor="code"
 aliases_file=$dotfile_dir/aliases.sh
@@ -11,18 +9,13 @@ alias r="refresh"
 
 # Function to create new aliases
 function new-alias() {
-  echo $aliases_file
-
   if [ -z "$1" ] || [ -z "$2" ]; then
     echo "${red}Invalid alias provided${reset}"
   else
-    ALIAS="alias ${1}=\"${2}\""
-    echo ""
-    echo ""
+    local ALIAS="alias ${1}=\"${2}\""
     echo $ALIAS >> $aliases_file
-    echo "${green}Alias: ${purple}${1}${green} created!"
-    echo ""
-    refresh
+    echo "${green}Alias: ${pink}${1}${green}=${purple}\"${2}\"${green} created!\n"
+    source $aliases_file
   fi
 }
 alias na="new-alias"
@@ -32,8 +25,10 @@ function bookmark() {
   if [ -z "$1" ]; then
     echo 'No name provided'
   else
-    echo "${green}alias ${1}=\"$(pwd)\" ${purple} created!"
-    echo "alias ${1}=\"cd $(pwd)\"" >> $aliases_file
+    local current_dir=$(pwd | sed 's/ /\\ /g')
+    echo "alias ${1}=\"cd ${current_dir}\"" >> $aliases_file
+    source $aliases_file
+    echo "${green}Alias: ${pink}${1}${green}=${purple}\"${current_dir}\"${green} created!\n"
   fi
 }
 alias b="bookmark"
@@ -51,7 +46,7 @@ alias w="warning"
 
 # Prints aliases if it exists
 function print-alias() {
-  ALIAS=$(grep "${1}" ~/dotfiles/aliases.sh)
+  local ALIAS=$(grep "${1}" ~/dotfiles/aliases.sh)
   
   if [ -z "$ALIAS" ]; then
     echo "${red}No alias found "
