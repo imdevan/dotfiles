1673331a821464cc4a86f478599242c95d403037
Devan Huapaya <huapayadevan@gmail.com>
2025-06-13 19:20:06 -0700

    update aliases

--- aliases.sh
+++ aliases.sh
@@ -17,14 +17,14 @@ alias a="aliases"
 alias dots="~/dotfiles"
 
 # Terminal Navigation
-alias la="ls -a"    # List all in directory
-alias o="open ./"   # Open - in finder
-alias cpr="cp -rf"  # Copy recursive
-alias rf="rm -rf"   # Remove recursive
-alias refresh="source ~/.zshrc && clear"    # Refresh the terminal
-alias r="refresh"                           # Refresh shortcut
-alias t="touch"     # Make file
-alias ex="exit"     # Exit terminal
+alias la="ls -a"                         # List all in directory
+alias o="open ./"                        # Open - in finder
+alias cpr="cp -rf"                       # Copy recursive
+alias rf="rm -rf"                        # Remove recursive
+alias refresh="source ~/.zshrc && clear" # Refresh the terminal
+alias r="refresh"                        # Refresh shortcut
+alias t="touch"                          # Make file
+alias ex="exit"                          # Exit terminal
 
 # Home Brew
 alias bri="brew install"
@@ -42,6 +42,14 @@ alias bu="bun uninstall"
 alias halp="cat ~/dotfiles/aliases.sh"
 alias dot="cd $dotfile_dir"
 
+alias hotkeys="v ~/dotfiles/docs/keybinds/hotkeys.md"
+alias hk="hotkeys"
+
+# Docker
+alias dcd="docker compose down"
+alias dcu="docker compose up"
+alias dcw="docker compose watch"
+
 # Code editors
 alias c="code ."
 alias cu="cursor ."
@@ -53,16 +61,16 @@ alias p="cd ~/Documents/Projects/"
 alias pp="cd /Users/devin/Documents/Projects/portfolio\ v5"
 alias play="cd /Users/devin/Documents/Projects/playground"
 
-# Git 
+# Git
 
 # Overhead
-alias gco="git checkout"        # Git checkout
-alias gcof="git checkout --"    # Checkout file
+alias gco="git checkout"     # Git checkout
+alias gcof="git checkout --" # Checkout file
 alias conflicts="git diff --name-only --diff-filter=U"
-alias no-edit="git commit --amend --no-edit"    # Does not push
-alias last="git log -1"                         # Show last change
-alias rebase="git fetch upstream && git rebase upstream/master"             # Fetch and rebase
-alias gcane!="git add -A && git commit --amend --no-edit && git push -f"    # Git commit ammend no edit and force push
+alias no-edit="git commit --amend --no-edit"                             # Does not push
+alias last="git log -1"                                                  # Show last change
+alias rebase="git fetch upstream && git rebase upstream/master"          # Fetch and rebase
+alias gcane!="git add -A && git commit --amend --no-edit && git push -f" # Git commit ammend no edit and force push
 alias grc="git rebase --continue"
 alias gfu="git fetch upstream"
 alias stash="git stash"
@@ -86,10 +94,11 @@ alias sparkles="git add . && git commit -m ':sparkles: ' && git push"
 alias status="git status"
 alias gcm="git commit -m"
 alias grm="git rebase main"
-alias gr="git checkout ."   # Reset a file
+alias gr="git checkout ." # Reset a file
 alias clone="git clone"
-alias gc="git clone"    # Calls native git clone
-alias cl="git-clone"    # Calls git-clone function (clone and cd)
+alias gc="git clone" # Calls native git clone
+alias cl="git-clone" # Calls git-clone function (clone and cd)
+alias gra="git remote add"
 
 # Branch shortcuts
 alias s="git checkout staging"
@@ -99,7 +108,6 @@ alias m="git checkout master"
 # Quick pushes
 alias wip="gap ':construction:'"
 alias gcat="gap ':octocat:'"
-
 
 # Ghostty
 alias gho="ghostty"
@@ -120,12 +128,12 @@ alias naf="npm audit fix"
 alias jc="npx jest --coverage"
 
 # PNPM
-alias ps="pnpm start"
-alias pd="pnpm dev"
-alias pi="pnpm install"
-alias pn="pnpm"
-alias pb="pnpm build"
-alias pr="pnpm run"
+alias pns="pnpm start"
+alias pnd="pnpm dev"
+alias pni="pnpm install"
+alias pnn="pnpm"
+alias pnb="pnpm build"
+alias pnr="pnpm run"
 
 # Google docs
 alias doc="python3 -mwebbrowser https://docs.google.com/create" # New google doc
@@ -135,7 +143,11 @@ alias docs="python3 -mwebbrowser http://docs.google.com"        # Main docs fold
 alias s="python3 -m http.server"
 
 # Python
+alias ve="source .venv/bin/activate"
 alias py="python3"
+
+# FastAPI
+alias fd="fastapi dev app/main.py"
 
 # Clean Modules (and run yarn)
 alias clean="rm -rf node_modules && yarn"
@@ -172,3 +184,4 @@ alias ytg="yarn test grep"
 alias yi="yarn install"
 
 # New
+alias fsft="cd /Users/devin/Documents/Projects/playground/full-stack-fastapi-template"
