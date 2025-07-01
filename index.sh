e61f5520e9bee177bb413904d8f00ebec0d2cd6e
Devan Huapaya <huapayadevan@gmail.com>
2025-07-01 11:08:18 -0700

    add aerospace config

--- index.sh
+++ index.sh
@@ -1,6 +1,9 @@
 # Change this is you change the location of these dotfiles
 dotfile_dir=~/dotfiles
 
+# Uncomment if overriding .zshrc
+source $dotfile_dir/config/zshrc.sh
+
 # Variables used everywhere
 source $dotfile_dir/vars/colors.sh
 
@@ -9,4 +12,3 @@ source $dotfile_dir/functions/index.sh
 
 # A bunch of aliases
 source $dotfile_dir/aliases.sh
-
