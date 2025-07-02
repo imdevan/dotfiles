c7998293c97e509d0bea8f37520d15db1c3c5062
devy <devy@MacBook-Pro.local>
2025-07-01 19:39:04 -0700

    update nvim color scheme

--- config/stow/nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ config/stow/nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,12 +1,25 @@
 return {
-  -- add gruvbox
-  { "ellisonleao/gruvbox.nvim" },
-
-  -- Configure LazyVim to load gruvbox
   {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "gruvbox",
-    },
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+      require("catppuccin").setup({
+        flavour = "macchiato", -- latte, frappe, macchiato, mocha
+        integrations = {
+          treesitter = true,
+          lsp_saga = true,
+          cmp = true,
+          gitsigns = true,
+          telescope = true,
+          notify = true,
+          mini = true,
+          native_lsp = {
+            enabled = true,
+          },
+        },
+      })
+      vim.cmd.colorscheme("catppuccin")
+    end,
   },
 }
