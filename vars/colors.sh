4da11fe9170e40b228f59dd5fff4bd579f187056
devy <devy@MacBook-Pro.local>
2025-07-07 11:51:37 -0700

    update colors vars

--- vars/colors.sh
+++ vars/colors.sh
@@ -3,10 +3,16 @@ red=`tput setaf 1`
 green=`tput setaf 2`
 yellow=`tput setaf 3`
 blue=`tput setaf 4`
+mauve=`tput setaf 5`
 teal=`tput setaf 6`
+white=`tput setaf 7`
+gray=`tput setaf 8`
 orange=`tput setaf 9`
+light_green=`tput setaf 10`
+gold=`tput setaf 11`
 purple=`tput setaf 12`
 pink=`tput setaf 13`
+light_blue=`tput setaf 14`
 bold=`tput bold`
 reset=`tput sgr0`
 
