d368e79358410741af1db1c562f0f7ac08009d19
devy <devy@MacBook-Pro.local>
2025-07-08 13:18:30 -0700

    update sketchybar config

--- config/stow/sketchybar/.config/sketchybar/plugins/aerospace.sh
+++ config/stow/sketchybar/.config/sketchybar/plugins/aerospace.sh
@@ -6,10 +6,14 @@
 # Currently undecided if i want to do border highlight or label color highlight
 
 if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
-  sketchybar --set $NAME background.drawing=on 
+  sketchybar --set $NAME drawing=on 
+  # sketchybar --set $NAME label.drawing=on 
+  # sketchybar --set $NAME background.drawing=on 
   # sketchybar --set $NAME label.color=0xfff74f9e
   # sketchybar --set $NAME label.color=0xffce02d0
 else
-  sketchybar --set $NAME background.drawing=off 
+  sketchybar --set $NAME drawing=off 
+  # sketchybar --set $NAME label.drawing=off 
+  # sketchybar --set $NAME background.drawing=off 
   # sketchybar --set $NAME label.color=0xffffffff
 fi
