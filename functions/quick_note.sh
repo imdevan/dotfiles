d7cad638241b8d98a0e353b0fc39f2f5757ad5a9
devy <devy@MacBook-Pro.local>
2025-07-12 21:41:42 -0700

    update quick_note functions

--- functions/quick_note.sh
+++ functions/quick_note.sh
@@ -11,9 +11,54 @@ function quick_note() {
     # - pass an argument to specify the name of the note
     # - pass an argument as a string containing the note
     # local note_file="~/Documents/notes/$(date +%Y-%m-%d).md"
-    
-    nvim ~/Documents/notes/$(date +%Y-%m-%d).txt
+   
+  if [ "$#" -lt 1 ]; then
+    nvim ~/Documents/notes/$(date +%d-%m-%Y).md
+  else  
+    nvim ~/documents/notes/$(to_snake_case "$@").md
+  fi
 }
 
 # Create alias
 alias qn="quick_note"
+
+
+# Function: notesbash function to print a list of files
+# Description: shows notes in notes folder
+# Usage: notes
+
+function notes() {
+  local folder=~/Documents/notes/
+
+  local named=()
+  local dated=()
+
+  for file in "$folder"*; do
+    [ -f "$file" ] || continue
+    local basename=$(basename "$file")
+
+    if [[ "$basename" =~ ^([0-9]{4}-[0-9]{2}-[0-9]{2}|[0-9]{2}-[0-9]{2}-[0-9]{4})\.(txt|md)$ ]]; then
+      dated+=("${basename%.*}")
+    else
+      # Remove extension, replace underscores with spaces, and title case
+      local clean_name="${basename%.*}"
+      clean_name="${clean_name//_/ }"
+      clean_name="$(echo "$clean_name" | awk '{for(i=1;i<=NF;++i) $i=toupper(substr($i,1,1)) tolower(substr($i,2)); print}')"
+      named+=("$clean_name")
+    fi
+  done
+
+  named_notes=$(printf "%s\n" "${named[@]}")
+  dated_notes=$(printf "%s\n" "${dated[@]}")
+
+  echo ""
+  echo "Named:"
+  echo "--------------------------------"
+  echo -e "${yellow}${named_notes}${reset}"
+  echo "--------------------------------"
+  echo ""
+  echo "Dated:"
+  echo "--------------------------------"
+  echo -e "${green}${dated_notes}${reset}"
+  echo "--------------------------------"
+}
