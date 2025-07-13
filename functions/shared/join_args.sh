cabf0d9135200fde033362ac769e3fce3095539b
devy <devy@MacBook-Pro.local>
2025-07-12 21:44:40 -0700

    add to_snake_case

--- functions/shared/join_args.sh
+++ functions/shared/join_args.sh
@@ -2,5 +2,12 @@
 # Description: Join multiple arguments with spaces
 # Usage: join_args [arguments]
 function join_args() {
-    echo "$@" | sed 's/ /+/g'
+  echo "$@" | sed 's/ /+/g'
 }
+
+to_snake_case() {
+  local input="$*"
+  local output=$(echo "$input" | tr '[:upper:]' '[:lower:]' | tr ' ' '_')
+  echo "$output"
+}
+
