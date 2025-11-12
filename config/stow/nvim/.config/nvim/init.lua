6abf4877ae733a9ebbc8d5947be4e25695377c3a
imdevan <huapayadevan@gmail.com>
2025-11-12 13:59:08 -0800

    nvim vscode extension don't load theme

--- config/stow/nvim/.config/nvim/init.lua
+++ config/stow/nvim/.config/nvim/init.lua
@@ -1,3 +1,10 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-vim.cmd("colorscheme catppuccin-macchiato")  -- or your preferred theme
+
+if not vim.g.vscode then
+  -- Load colorscheme and UI-related plugins
+  vim.cmd("colorscheme catppuccin-macchiato") -- or your preferred theme
+end
+
+-- vim.opt.spellfile = "~/.config/nvim/spell/en.utf-8.add"
+-- vim.opt.spellfile = "~/.config/nvim/spell"
