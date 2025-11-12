0a8bd03f9612413dd94bf314e1453be5c41245f5
imdevan <huapayadevan@gmail.com>
2025-11-12 13:58:32 -0800

    nvim add url-open plugin

---
+++ config/stow/nvim/.config/nvim/lua/plugins/url-open.lua
@@ -0,0 +1,20 @@
+-- Plugin to open urls in browser. Also works with certain plugins
+-- https://github.com/sontungexpt/url-open
+
+-- Keymap
+vim.keymap.set("n", "gx", "<esc>:URLOpenUnderCursor<cr>")
+
+-- Plugin config
+return {
+  "sontungexpt/url-open",
+  event = "VeryLazy",
+  branch = "mini", -- minimal source with no comments, no validate configs, no documents
+  cmd = "URLOpenUnderCursor",
+  config = function()
+    local status_ok, url_open = pcall(require, "url-open")
+    if not status_ok then
+      return
+    end
+    url_open.setup({})
+  end,
+}
