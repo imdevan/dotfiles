24e1e10230e958a845a6787fe8e43a3be15fdb0a
imdevan <huapayadevan@gmail.com>
2025-11-17 01:12:17 -0800

    update sketchybar config

--- config/stow/sketchybar/.config/sketchybar/plugins/front_app.sh
+++ config/stow/sketchybar/.config/sketchybar/plugins/front_app.sh
@@ -4,7 +4,62 @@
 # variable. E.g. the front_app_switched event sends the name of the newly
 # focused application in the $INFO variable:
 # https://felixkratz.github.io/SketchyBar/config/events#events-and-scripting
+#
+SUBSTITUTES="
+chrome=🦄
+arc=📺
+iterm2=👾
+code=🧑🏻‍💻
+kiro=👻
+cursor=🐁
+keyboard=⌨️
+karabiner=🧗🏻‍♀️
+system=⚙️
+"
+
+lower() {
+  printf '%s' "$1" | tr 'A-Z' 'a-z'
+}
+
+substitute() {
+  name_lc="$(lower "$1")"
+  found=false
+  result=""
+
+  # read $SUBSTITUTES in the current shell
+  while IFS= read -r line; do
+    # skip empty lines or comments
+    case "$line" in
+    '' | \#*) continue ;;
+    esac
+
+    key="${line%%=*}"
+    val="${line#*=}"
+
+    # lowercase key for case-insensitive matching
+    key_lc="$(lower "$key")"
+
+    case "$name_lc" in
+    *"$key_lc"*)
+      result="$val"
+      # result="$name_lc $val"
+      found=true
+      break
+      ;;
+    esac
+  done <<EOF
+$SUBSTITUTES
+EOF
+
+  if [ "$found" = true ]; then
+    printf '%s\n' "$result"
+  else
+    printf '%s\n' "$name_lc"
+  fi
+}
 
 if [ "$SENDER" = "front_app_switched" ]; then
-  sketchybar --set "$NAME" label="$INFO"
+  label="$(substitute "$INFO")"
+
+  sketchybar --set "$NAME" label="$label"
 fi
