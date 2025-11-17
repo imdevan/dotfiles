d559fc70f5a33d448bbc256b6192c883bbfd54a7
imdevan <huapayadevan@gmail.com>
2025-11-16 23:00:05 -0800

    add wip load_preset to aerospace

---
+++ config/stow/aerospace/load_preset.sh
@@ -0,0 +1,15 @@
+# TODO:
+# this script should load in different pre saved workspace layouts
+# either from a seperate file, or saved in this file to start
+#
+# if hard coded start with 3 basic layouts
+#
+# work:
+#
+# 1. browser 500px wide | terminal (preferablly with tmux started and resurected)
+#
+# 2.  discord | slack
+#            ---
+#    messages | whatsapp
+#
+#
