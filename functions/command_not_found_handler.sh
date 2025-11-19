925c4267a111e3a4b3bebdc1d542ebdac0e5b430
imdevan <huapayadevan@gmail.com>
2025-11-19 13:49:38 -0800

    update command not found

--- functions/command_not_found_handler.sh
+++ functions/command_not_found_handler.sh
@@ -8,10 +8,8 @@
 command_not_found_handler() {
   # $1 is the command name typed
   # $@ is the whole argument list
-
-  cowsay -r "${red}Command not found :(${reset}"
+  echo -e "${mauve}\"$1\"${reset}\nCommand not found :(${reset}" | cowsay -n
   echo "\n${yellow}Checking for aliases...${reset}\n"
-
   is_alias "$1"
 }
 
