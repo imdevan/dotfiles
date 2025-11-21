7a4a9777665c69268410c454c46cf406e06a04aa
imdevan <huapayadevan@gmail.com>
2025-11-20 21:30:15 -0800

    update qutebrowser config

--- config/stow/qutebrowser/.qutebrowser/config.py
+++ config/stow/qutebrowser/.qutebrowser/config.py
@@ -2,7 +2,8 @@ import catppuccin
 
 config.load_autoconfig()
 
-path_to_start_page = "https://github.com/news-s/Qutebrowser-config/blob/main/config.py"
+path_to_start_page = "https://google.com/"
+# path_to_start_page = "https://github.com/news-s/Qutebrowser-config/blob/main/config.py"
 # path_to_start_page = "https://qutebrowser.org/doc/quickstart.html"
 
 c.window.hide_decoration = True
@@ -19,22 +20,50 @@ c.url.start_pages = path_to_start_page
 
 c.content.fullscreen.window = True
 
+c.url.searchengines = {
+    "DEFAULT": "https://google.com/search?q={}",
+    "yt": "https://www.youtube.com/results?search_query={}",
+    "git": "https://github.com/search?q={}",
+    "stack": "https://stackoverflow.com/search?q={}",
+    "lol": "u.gg/lol/champions/{}/build",
+}
+
+config.bind("J", "tab-prev")
+config.bind("K", "tab-next")
+config.bind("X", "undo")
+config.bind("<Ctrl-t>", f"open -t {path_to_start_page}")
+
+c.aliases["chat"] = "open -t https://chat.openai.com"
+
+c.content.javascript.enabled = True
+c.content.autoplay = False
+c.content.cookies.accept = "no-3rdparty"
+c.content.geolocation = False
+c.content.notifications.enabled = False
+
+c.content.blocking.enabled = True
+c.content.blocking.method = "both"
+c.content.blocking.adblock.lists = [
+    "https://easylist.to/easylist/easylist.txt",
+    "https://easylist.to/easylist/easyprivacy.txt",
+    "https://secure.fanboy.co.nz/fanboy-cookiemonster.txt",
+]
+
+c.editor.command = ["code", "{}"]
 # Theme config
 # set the flavor you'd like to use
 # valid options are 'mocha', 'macchiato', 'frappe', and 'latte'
 # last argument (optional, default is False): enable the plain look for the menu rows
-catppuccin.setup(c, 'macchiato', True)
+catppuccin.setup(c, "macchiato", True)
 
 
-# docs: 
+# docs:
 # https://qutebrowser.org/doc/help/configuring.html
 # https://qutebrowser.org/doc/quickstart.html
 # https://github.com/tinted-theming/base16-qutebrowser
+# https://github.com/jtyers/qutebrowser-profile/blob/master/qutebrowser-profile
 #
-# default config: 
-# https://gist.github.com/nightgrey/2ed92d362077843502e3dce639aae9c7
 #
 # inspiration:
 # https://github.com/news-s/Qutebrowser-config/blob/main/config.py
-# https://github.com/jtyers/qutebrowser-profile/blob/master/qutebrowser-profile
 #
