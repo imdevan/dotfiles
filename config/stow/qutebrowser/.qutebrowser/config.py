3880901a0821684fe756c5913f82be24b5bb95f3
imdevan <huapayadevan@gmail.com>
2025-11-21 17:38:39 -0800

    update qutebrowser config

--- config/stow/qutebrowser/.qutebrowser/config.py
+++ config/stow/qutebrowser/.qutebrowser/config.py
@@ -1,14 +1,28 @@
+# config file docs
+# https://qutebrowser.org/doc/help/configuring.html
+# https://github.com/news-s/Qutebrowser-config/blob/main/config.py
+# https://qutebrowser.org/doc/help/settings.html
+
 import catppuccin
 
+# Must load first
 config.load_autoconfig()
 
 path_to_start_page = "http://localhost:8081/"
 # path_to_start_page = "https://github.com/news-s/Qutebrowser-config/blob/main/config.py"
 # path_to_start_page = "https://qutebrowser.org/doc/quickstart.html"
 
+# Keybinds
+config.bind("J", "tab-prev")
+config.bind("K", "tab-next")
+config.bind("X", "undo")
+config.bind("<Cmd-Alt-i>", "devtools right")
+config.bind("<Cmd-Alt-b>", "devtools bottom")
+config.bind("<Ctrl-t>", f"open {path_to_start_page}")
+
+# Window Styles
 c.window.hide_decoration = True
 c.window.transparent = True
-
 c.colors.webpage.darkmode.enabled = True
 c.colors.webpage.preferred_color_scheme = "dark"
 c.statusbar.show = "in-mode"
@@ -16,10 +30,9 @@ c.tabs.show = "switching"
 c.tabs.position = "top"
 c.tabs.favicons.show = "pinned"
 c.url.start_pages = path_to_start_page
-
-
 c.content.fullscreen.window = True
 
+# Search Engines
 c.url.searchengines = {
     "DEFAULT": "https://google.com/search?q={}",
     "yt": "https://www.youtube.com/results?search_query={}",
@@ -28,19 +41,17 @@ c.url.searchengines = {
     "lol": "u.gg/lol/champions/{}/build",
 }
 
-config.bind("J", "tab-prev")
-config.bind("K", "tab-next")
-config.bind("X", "undo")
-config.bind("<Ctrl-t>", f"open -t {path_to_start_page}")
-
+# Aliases
 c.aliases["chat"] = "open -t https://chat.openai.com"
 
+# Defaults
 c.content.javascript.enabled = True
 c.content.autoplay = False
 c.content.cookies.accept = "no-3rdparty"
 c.content.geolocation = False
 c.content.notifications.enabled = False
 
+# Adblock
 c.content.blocking.enabled = True
 c.content.blocking.method = "both"
 c.content.blocking.adblock.lists = [
@@ -49,21 +60,23 @@ c.content.blocking.adblock.lists = [
     "https://secure.fanboy.co.nz/fanboy-cookiemonster.txt",
 ]
 
+# Code Editor
 c.editor.command = ["code", "{}"]
-# Theme config
+
+# Theme Config
 # set the flavor you'd like to use
 # valid options are 'mocha', 'macchiato', 'frappe', and 'latte'
 # last argument (optional, default is False): enable the plain look for the menu rows
 catppuccin.setup(c, "macchiato", True)
 
 
-# docs:
+# Docs:
 # https://qutebrowser.org/doc/help/configuring.html
 # https://qutebrowser.org/doc/quickstart.html
 # https://github.com/tinted-theming/base16-qutebrowser
 # https://github.com/jtyers/qutebrowser-profile/blob/master/qutebrowser-profile
 #
 #
-# inspiration:
+# Inspiration:
 # https://github.com/news-s/Qutebrowser-config/blob/main/config.py
 #
