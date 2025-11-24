a5b59a5a24a28497b74c67bd7affd3468956120a
imdevan <huapayadevan@gmail.com>
2025-11-24 14:13:28 -0800

    update cnf handler

--- functions/command_not_found_handler.sh
+++ functions/command_not_found_handler.sh
@@ -1,17 +1,13 @@
 #!/bin/sh
 
 # Function: command_not_found_handler
-# Description: [Add description here]
-# Usage: command_not_found_handler [arguments]
-# Alias: cnfh
+# Description: function that runs when a command is not found
+# Usage: command_not_found_handler [bad_command]
 
 command_not_found_handler() {
   # $1 is the command name typed
   # $@ is the whole argument list
   echo -e "${mauve}\"$1\"${reset}\nCommand not found :(${reset}" | cowsay -n
-  echo "\n${yellow}Checking for aliases...${reset}\n"
+  echo "\n${yellow}Checking for aliases... Ctrl + C to cancel${reset}\n"
   is_alias "$1"
 }
-
-# Create alias
-alias cnfh="command_not_found_handler"
