dd93c280d4c144a9ee7e0ca1141547473229f6ee
imdevan <huapayadevan@gmail.com>
2025-12-11 12:58:14 -0800

    moved to_copy to shared functions

--- functions/index.sh
+++ functions/index.sh
@@ -1,9 +1,17 @@
+# Auto-compile all function files on first load or when changed
+for func_file in $dotfile_dir/functions/**/*.sh(N); do
+    if [[ ! -f "${func_file}.zwc" ]] || [[ "$func_file" -nt "${func_file}.zwc" ]]; then
+        zcompile "$func_file" 2>/dev/null
+    fi
+done
+
 # Shared functions
 source $dotfile_dir/functions/shared/colors.sh # colors should be loaded first
 source $dotfile_dir/functions/shared/validators.sh
 source $dotfile_dir/functions/shared/join_args.sh
 source $dotfile_dir/functions/shared/has_flag.sh
 source $dotfile_dir/functions/shared/alias_utils.sh
+source $dotfile_dir/functions/shared/to_copy.sh
 
 # Global function handler for zsh
 source $dotfile_dir/functions/command_not_found_handler.sh
@@ -22,7 +30,6 @@ source $dotfile_dir/functions/search.sh
 source $dotfile_dir/functions/npm_latest.sh
 source $dotfile_dir/functions/post.sh
 source $dotfile_dir/functions/make_directory.sh
-source $dotfile_dir/functions/to_copy.sh
 source $dotfile_dir/functions/simulator.sh
 source $dotfile_dir/functions/to_send.sh
 source $dotfile_dir/functions/gpt.sh
@@ -49,3 +56,9 @@ source $dotfile_dir/functions/fcd.sh
 source $dotfile_dir/functions/aero_utils.sh
 
 source $dotfile_dir/functions/ytdl.sh
+
+source $dotfile_dir/functions/multi-agent-search.sh
+
+source $dotfile_dir/functions/sh_utils.sh
+
+source $dotfile_dir/functions/docker_utils.sh
