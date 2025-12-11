826124623d8e5a86d68d07488953aad509937d12
imdevan <huapayadevan@gmail.com>
2025-12-11 13:00:18 -0800

    performance: cache function and aliases

--- index.sh
+++ index.sh
@@ -1,6 +1,14 @@
 # Change this is you change the location of these dotfiles
 dotfile_dir=~/dotfiles
 
+# Add to top of ~/dotfiles/index.sh
+for file in $dotfile_dir/functions/**/*.sh(N) $dotfile_dir/aliases.sh; do
+    if [[ ! -f "${file}.zwc" ]] || [[ "$file" -nt "${file}.zwc" ]]; then
+        zcompile "$file" 2>/dev/null
+    fi
+done
+
+# Removed in favor of caching
 # Import functions
 source $dotfile_dir/functions/index.sh
 
