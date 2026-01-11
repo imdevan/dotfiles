9061dd9952422dd2cb37c6184769896e23015f8b
imdevan <huapayadevan@gmail.com>
2026-01-10 19:20:23 -0800

    update snacks config

--- config/stow/nvim/.config/nvim/lua/plugins/snacks.lua
+++ config/stow/nvim/.config/nvim/lua/plugins/snacks.lua
@@ -1,3 +1,6 @@
+-- https://github.com/folke/snacks.nvim/tree/main/docs
+-- https://github.com/folke/snacks.nvim/blob/main/docs/styles.md
+
 -- additional snacks
 return {
   "folke/snacks.nvim",
@@ -15,6 +18,28 @@ return {
           hidden = true,
           ignored = false,
         },
+      },
+    }
+
+    opts.terminal = {
+      win = {
+        style = "float",
+        -- width = math.floor(vim.o.columns * 0.5),
+        -- height = math.floor(vim.o.lines * 0.5),
+        -- height = 1,
+        -- height = 10,
+        height = 15,
+        -- height = 20,
+        width = math.floor(vim.o.columns * 0.5),
+        border = "rounded",
+      },
+    }
+
+    opts.lazygit = {
+      win = {
+        border = false,
+        height = math.floor(vim.o.columns),
+        width = math.floor(vim.o.columns),
       },
     }
 
