df43ec4ea2fe493e77cccb0a735586a26a1d2288
imdevan <huapayadevan@gmail.com>
2026-01-14 18:37:08 -0800

    update zen mode

--- config/stow/nvim/.config/nvim/lua/plugins/snacks.lua
+++ config/stow/nvim/.config/nvim/lua/plugins/snacks.lua
@@ -38,12 +38,38 @@ return {
     opts.lazygit = {
       win = {
         border = false,
-        height = math.floor(vim.o.columns),
+        height = math.floor(vim.o.lines),
         width = math.floor(vim.o.columns),
       },
     }
 
     -- Disable indent guide lines
     opts.indent = { enabled = false }
+    opts.zen = {
+      -- You can add any `Snacks.toggle` id here.
+      -- Toggle state is restored when the window is closed.
+      -- Toggle config options are NOT merged.
+      ---@type table<string, boolean>
+      toggles = {
+        dim = false,
+        git_signs = true,
+        mini_diff_signs = true,
+        -- diagnostics = false,
+        -- inlay_hints = false,
+      },
+      win = {
+        height = math.floor(vim.o.lines - 6),
+        width = 180,
+        -- minimal = true,
+      },
+      on_open = function()
+        vim.wo.number = false
+        vim.wo.relativenumber = false
+      end,
+      on_close = function()
+        vim.wo.number = true
+        vim.wo.relativenumber = true
+      end,
+    }
   end,
 }
