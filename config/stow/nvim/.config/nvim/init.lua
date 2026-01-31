b01a5ff04618b6585ab4f404ba98a13bc692f494
Devan Huapaya <huapayadevan@gmail.com>
2026-01-30 16:45:04 -0800

    update: comment out color scheme in nvim init

--- config/stow/nvim/.config/nvim/init.lua
+++ config/stow/nvim/.config/nvim/init.lua
@@ -1,10 +1,11 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
-if not vim.g.vscode then
-  -- Load colorscheme and UI-related plugins
-  vim.cmd("colorscheme catppuccin") -- or your preferred theme
-end
+-- todo: configure aether style setup on macos
+-- if not vim.g.vscode then
+--   -- Load colorscheme and UI-related plugins
+--   vim.cmd("colorscheme catppuccin-macchiato") -- or your preferred theme
+-- end
 
 -- vim.opt.spellfile = "~/.config/nvim/spell/en.utf-8.add"
 -- vim.opt.spellfile = "~/.config/nvim/spell"
