2ccb1568052f17de6673727d142cbaaf11becacb
Devan Huapaya <huapayadevan@gmail.com>
2026-02-07 00:15:45 -0800

    add omni_copy

--- functions/shared/to_copy.sh
+++ functions/shared/to_copy.sh
@@ -1,12 +1,27 @@
 #!/bin/sh
 
+# Function: omni_copy
+# Description: Cross-platform copy to clipboard (pbcopy on macOS, wl-copy elsewhere)
+# Usage: omni_copy [text]
+# Alias: oc
+
+function omni_copy() {
+  if [[ "$OSTYPE" == "darwin"* ]]; then
+    pbcopy
+  else
+    wl-copy
+  fi
+}
+
+alias oc="omni_copy"
+
 # Function: to_copy
 # Description: Copy output
 # Usage: copy [arguments]
 # Alias: c
 
 function to_copy() {
-  "$@" | pbcopy
+  "$@" | omni_copy
 }
 
 # Create alias
@@ -14,13 +29,13 @@ alias tc="to_copy"
 
 
 function copy_text() {
-  echo "$@" | pbcopy
+  echo "$@" | omni_copy
 }
 
 alias ct="copy_text"
 
 function copy_pwd() {
-  pwd | sed "s|^$HOME|~|" | pbcopy
+  pwd | sed "s|^$HOME|~|" | omni_copy
 }
 
 # Create alias
