c79fbc340e3a1f6500f5a130f9d92456ecdfe969
Devan Huapaya <huapayadevan@gmail.com>
2026-02-13 00:49:05 -0800

    add togglr plugin

---
+++ config/stow/nvim/.config/nvim/lua/plugins/togglr.lua
@@ -0,0 +1,19 @@
+return {
+  "elentok/togglr.nvim",
+  opts = {
+    -- Specify key map (set to false or nil to disable)
+    -- key = "<Leader>pj",
+    -- Set in keymaps!
+
+    -- Specify which register to use (to avoid overriding the default register)
+    register = "t",
+
+    -- Enable debugging mode
+    -- debug = true,
+
+    -- Add custom sets to values to toggle between
+    -- values = {
+    --   ["value"] = "opposite-value",
+    -- },
+  },
+}
