5509cc64378035b06a1b36091a53336879e86a06
Devan Huapaya <huapayadevan@gmail.com>
2026-05-30 19:46:08 -0700

    update hypr config

--- config/stow/hypr/.config/hypr/scripts/get-to-work.sh
+++ config/stow/hypr/.config/hypr/scripts/get-to-work.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
-uwsm-app -- ghostty -e zsh -ic 'dp; exec zsh' &
+uwsm-app -- ghostty -e zsh -ic 'tr; exec zsh' &
 uwsm-app -- qutebrowser &
 sleep 2
 hyprctl dispatch splitratio exact 1.33
