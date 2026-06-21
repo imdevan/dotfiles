85b918ea0e64f3e4b0cd56b31fc0d9d166e2ea16
Devan Huapaya <huapayadevan@gmail.com>
2026-06-20 18:31:19 -0700

    remove compiled file check

--- index.sh
+++ index.sh
@@ -1,17 +1,9 @@
 # Change this is you change the location of these dotfiles
 dotfile_dir=~/dotfiles
 
-# Add to top of ~/dotfiles/index.sh
-for file in $dotfile_dir/functions/**/*.sh(N) $dotfile_dir/aliases.sh; do
-    if [[ ! -f "${file}.zwc" ]] || [[ "$file" -nt "${file}.zwc" ]]; then
-        zcompile "$file" 2>/dev/null
-    fi
-done
-
 # A bunch of aliases
 source $dotfile_dir/aliases.sh
 
 # Removed in favor of caching
 # Import functions
 zsh-defer source $dotfile_dir/functions/index.sh
-
