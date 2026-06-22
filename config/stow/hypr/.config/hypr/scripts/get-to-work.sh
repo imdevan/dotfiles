7ef359a34551ddf63c0bf464148c24fa9f3b8e91
Devan Huapaya <huapayadevan@gmail.com>
2026-06-22 10:04:18 -0700

    rename recap tmux launch alias

--- config/stow/hypr/.config/hypr/scripts/get-to-work.sh
+++ config/stow/hypr/.config/hypr/scripts/get-to-work.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
-uwsm-app -- ghostty -e zsh -ic 'tr; exec zsh' &
+uwsm-app -- ghostty -e zsh -ic 'tre; exec zsh' &
 uwsm-app -- qutebrowser &
 sleep 2
 hyprctl dispatch splitratio exact 1.33
